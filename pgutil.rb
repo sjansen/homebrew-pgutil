23073b6dcb9b522d7de314cea9d618fba6835ad3
sjansen <sjansen@buscaluz.org>
2019-05-08 08:11:03 -0600

    Brew formula update for pgutil version v0.1.0-alpha.2

--- pgutil.rb
+++ pgutil.rb
@@ -2,9 +2,9 @@
 class Pgutil < Formula
   desc ""
   homepage ""
-  url "https://github.com/sjansen/pgutil/releases/download/v0.1.0-alpha.1/pgutil_0.1.0-alpha.1_darwin_amd64.tar.gz"
-  version "0.1.0-alpha.1"
-  sha256 "51963052f45f0396674f05bd94fa9bd15f6bb445d046a3bf54a9fe61182010e1"
+  url "https://github.com/sjansen/pgutil/releases/download/v0.1.0-alpha.2/pgutil_0.1.0-alpha.2_darwin_amd64.tar.gz"
+  version "0.1.0-alpha.2"
+  sha256 "b716419859ab1d2352d272cd626f507576a2ab257e019d2a8cc0bad9a8ab03a2"
 
   def install
     bin.install "pgutil"
